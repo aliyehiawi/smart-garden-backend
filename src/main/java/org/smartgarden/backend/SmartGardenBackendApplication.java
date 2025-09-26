c879ddcea4e375680f4156bf710afaa4fea8ae5c
aliyehiawi <ali.yehiawii@example.com>
2025-09-26 13:57:23 +0200

    initialize

---
+++ src/main/java/org/smartgarden/backend/SmartGardenBackendApplication.java
@@ -0,0 +1,13 @@
+package org.smartgarden.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartGardenBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartGardenBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/smartgarden/backend/SmartGardenBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.smartgarden.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartGardenBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
